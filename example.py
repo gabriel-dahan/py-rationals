2626cf7833774708ab6d0e07d2793db6f9e49c74
thegabdoosan <gabrieldahan05@gmail.com>
2023-10-25 21:17:26 +0200

    Release

--- example.py
+++ example.py
@@ -1,33 +1,39 @@
+from typing import List
+
 """ 
     Exemple d'exercice de travail sur les fractions pouvant utiliser la classe `rational` :
-
-    |  L'application f prend un rationnel x, écrit son écriture décimale et remplace tous les 5 par des 4,
+    
+    Exercice :
+    |  L'application f prend un rationnel x, écrit son écriture décimale, remplace tous les 5 par des 4
     |  et le remet sous forme rationnelle.
     |    1) Ecrire cette fonction en Python (à l'aide de rational).
     |    2) Calculer f(1/2), f(1/4), f(17/14). 
     |    3) Combien l'équation f(x)=73/495 a-t-elle de solutions ? Combien l'équation f(x)=541/990 a-t-elle de solutions ?
 """
 
-from typing import List
-from rationals import rational
-from random import randint
+# -- Pour lancer ce fichier, veillez à avoir installé la librairie suivante :
+from prettytable import PrettyTable # pip install --user prettytable
 
-# -- Veillez à avoir installé la suivante pour lancer ce fichier :
-from prettytable import PrettyTable
+from rationals import rational
 
 # -- 1) --
+# On s'autorise de rajouter un paramètre 'replace' à la fonction, spécifiant les valeurs à interchanger dans le développement décimal de x
+# (on utilise ce paramètre dans la question 3, pour faire la réciproque de f en prenant replace = (4, 5)).
 def f(x: rational, replace: tuple = (5, 4)) -> rational:
-    dec = x.as_decimal()
+    a, b = replace
+    dec, period = x.as_decimal() # Renvoie un tuple contenant la partie décimale finie de x et sa période si elle existe (sinon 0).
 
-    new_dec = str(dec[0]).replace(str(replace[0]), str(replace[1]))
-    new_dec = float(new_dec) if isinstance(dec[0], float) else int(new_dec)
+    new_dec = str(dec).replace(str(a), str(b))
+    # Spécificité de mon implémentation : une partie décimale égale à 1 n'est pas égale à 1.0. La précision est prise en compte, ainsi rational(4, 3) = 4/3 = 1.333... != 1.0333...
+    new_dec = float(new_dec) if isinstance(dec, float) else int(new_dec) 
 
-    new_period = int(str(dec[1]).replace(str(replace[0]), str(replace[1])))
+    new_period = int(str(period).replace(str(a), str(b)))
 
     return rational.from_decimal(new_dec, new_period)
 
 # -- 2) --
 def question_2():
+    # On fait une liste des valeurs à évaluer, puis on les évalue tout en les affichant dans un beau tableau créé par PrettyTable :)
     Lx: List[rational] = [
         rational(1, 2),
         rational(1, 4),
@@ -42,6 +48,8 @@ def question_2():
 
 # -- 3) --
 def question_3():
+    # De même que dans la question 2, à la seule différence ici qu'on prend la réciproque de f : replace = (4,5) à la place de (5, 4).
+    # On répond alors implicitement à la question posée : il n'y a qu'une solution à n'importe quelle équation posée avec f.
     Lx: List[rational] = [
         rational(73, 495),
         rational(541, 990)
@@ -53,4 +61,4 @@ def question_3():
     print(pt.get_string())
 
 if __name__ == '__main__':
-    print(rational(7, 8) + 1.65)
+    question_3()
